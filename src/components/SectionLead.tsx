12f6a074dd3d4903ce285cef71441a4fb15e3942
ila676383-web <ila676383@gmail.com>
2026-01-22 18:48:18 +0500

    v4

--- src/components/SectionLead.tsx
+++ src/components/SectionLead.tsx
@@ -14,8 +14,10 @@ const SectionLead = ({
   buttonText = "Записаться",
 }: SectionLeadProps) => {
   return (
-    <section className="mx-auto relative p-4 py-10 flex flex-col md:flex-row justify-between items-center px-6 md:px-20 gap-10 md:gap-20 max-w-7xl">
-      <div className="absolute bg-gray-300/30 w-[90%] mx-[5%] rounded-2xl h-full top-0 left-0 -z-10"></div>
+    <section
+      className="mx-auto relative p-4 py-10 flex flex-col md:flex-row justify-between items-center px-6 md:px-20 gap-10 md:gap-20 max-w-7xl
+    border-4 border-pink-300 bg-gray-300/30 shadow transition-shadow duration-300 ease-in-out hover:shadow-[0_0_15px_5px_rgba(219,39,119,0.6)] rounded-2xl"
+    >
       <div className="max-w-xl text-center md:text-left">
         <h1 className="text-3xl font-bold mb-6">{head}</h1>
         <p className="mb-8 text-lg leading-relaxed">{text}</p>
