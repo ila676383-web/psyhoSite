42e44ab159346502f92f0ed0b0244cfd4253e86e
ila676383-web <ila676383@gmail.com>
2026-01-22 17:54:57 +0500

    test v2

--- src/components/BtnFormBack.tsx
+++ src/components/BtnFormBack.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import btnForm from "./BtnBack.svg";
+import btnForm from "../../public/btnBack.svg";
 import { useState } from "react";
 import FormBack from "./FormBack";
 
