f8ef951f0047cea8d5c0079d9844b434db08a568
ila676383-web <ila676383@gmail.com>
2026-02-02 17:58:51 +0500

    v2.0.4

--- src/app/(home)/games/page.tsx
+++ src/app/(home)/games/page.tsx
@@ -3,27 +3,15 @@ import Image from "next/image";
 import frame1 from "../../../images/frame1.png";
 const page = () => {
   return (
-    <main className="container w-[80%] mx-auto min-h-screen flex flex-col  gap-20   ">
+    <main className="container w-[80%] mx-auto min-h-screen flex flex-col jus gap-20   \
+     ">
       <LeadStair />
 
-      {
-      /* <section className="flex flex-col justify-center items-start gap-6 px-4">
-        <h1 className="text-3xl font-bold">С какими запросами работают:</h1>
-        <ul className=" list-inside mt-4 space-y-2 cursor-pointer marker:text-2xl marker:text-purple-500 marker:font-bold marker:content-['▹']">
-          <li className="hover:font-medium">Отношения и самооценка</li>
-          <li className="hover:font-medium">Деньги и реализация</li>
-          <li className="hover:font-medium">Страхи и внутренние ограничения</li>
-          <li className="hover:font-medium">Жизненные выборы</li>
-        </ul>
-        <button className="bg-gray-300 py-3 px-5 rounded-2xl shadow-lg font-bold hover:scale-105 active:scale-95 transition-transform ease-in-out duration-500">
-          Зарегистрироваться на игру
-        </button>
-      </section> */
-      }
+   
 
-      <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+      <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 justify-items-center max-w-screen">
 
-        <div className=" w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
+        <div className=" max-w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
           <Image src={frame1} alt="Игра" width={"200"} height={"200"} />
           <h2>Название</h2>
           <p>
@@ -31,7 +19,7 @@ const page = () => {
           </p>
           <button>Записаться</button>
         </div>
-           <div className=" w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
+           <div className=" max-w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
           <Image src={frame1} alt="Игра" width={"200"} height={"200"} />
           <h2>Название</h2>
           <p>
@@ -39,7 +27,7 @@ const page = () => {
           </p>
           <button>Записаться</button>
         </div>
-           <div className=" w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
+           <div className=" max-w-100 bg-gray-50 flex flex-col justify-center items-center gap-6 p-10 rounded-3xl shadow-lg mx-auto ">
           <Image src={frame1} alt="Игра" width={"200"} height={"200"} />
           <h2>Название</h2>
           <p>
