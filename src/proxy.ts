9cc2514911712dcb665bf6b29feabf8b4ae869c0
ila676383-web <ila676383@gmail.com>
2026-02-13 14:22:20 +0500

    dev 2.0

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,12 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
   if (!token) {
     return NextResponse.redirect(new URL("/admin", req.url));
   }
+  
   try {
     const { payload } = await jwtVerify(token, secret);
     if (payload.role !== "admin") {
