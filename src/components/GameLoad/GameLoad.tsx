c9d439af3fbdcfdf362627742493ce5072875767
ila676383-web <ila676383@gmail.com>
2026-02-19 17:18:21 +0500

    dev 23.0

--- src/components/GameLoad/GameLoad.tsx
+++ src/components/GameLoad/GameLoad.tsx
@@ -5,19 +5,19 @@ import GameCard from "./GameCard";
 const GameLoad = async () => {
   const games: GameApi[] = await getGames();
   return (
-    
     <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10 justify-items-center max-w-screen">
       {games
         .sort((a, b) => b.id - a.id)
         .map((game: GameApi) => {
           return (
-            <GameCard
-              key={game.id}
-              id={game.id}
-              name={game.name}
-              description={game.description}
-              image={game.image}
-            />
+            // <GameCard
+            //   key={game.id}
+            //   id={game.id}
+            //   name={game.name}
+            //   description={game.description}
+            //   image={game.image}
+            //   />
+            <h1 key={game.id}>{game.name}</h1>
           );
         })}
     </section>
