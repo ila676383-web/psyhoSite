28c52c6dfb765e16e42604e46c5600898f037d65
ila676383-web <ila676383@gmail.com>
2026-02-19 18:22:07 +0500

    dev v35

--- src/components/animate/Stair.tsx
+++ src/components/animate/Stair.tsx
@@ -1,34 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Spline from "@splinetool/react-spline";
 
 
-
 export default function Stair() {
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    //  защита от SSR (на всякий случай)
-    if (typeof window === "undefined") return;
-
-
-    //  проверка WebGL
-    const canvas = document.createElement("canvas");
-    const gl =
-      canvas.getContext("webgl") ||
-      canvas.getContext("experimental-webgl");
-
-    if (!gl) return;
-
-    // 4. асинхронно, чтобы не было warning
-    requestAnimationFrame(() => {
-      setReady(true);
-    });
-  }, []);
-
-  if (!ready) return null;
-
   return (
     <div className="absolute top-0 right-0 w-full h-[600px] -z-10 opacity-40 pointer-events-none">
       <Spline scene="https://prod.spline.design/FlT781mt-IJmBGQN/scene.splinecode" />
